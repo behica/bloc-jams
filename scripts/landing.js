7feac5a0fa27d33cdae51ea134f7985fe67387ca
Behic Akgun <behica@gmail.com>
2017-02-10 12:35:49 -0600

    Assignment 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,5 @@
-var pointsArray = document.getElementsByClassName('point');
-
 var animatePoints = function(points){        
-        var revealPoint = function() {
-            for(var i = 0; i < points.length; i++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-            }
-        }
-        revealPoint();
+    forEach();
 };
 
 window.onload = function() {
@@ -21,7 +11,7 @@ window.onload = function() {
     
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-           animatePoints(pointsArray); 
+           animatePoints(points); 
         }
     });
 }
